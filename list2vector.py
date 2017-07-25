4b631be69315cfa87c10cbac64ede178a0af55e2
LiuSS <liuyuanxiu1218@gmail.com>
2017-07-25 22:54:42 +0800

    Create list2vector.py

---
+++ list2vector.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+
+class list2vector():
+    def __init__(self):
+        pass
+        
+    def int2int(self, input_list):
+        input_list_str = '{' + str(input_list).strip('[').strip(']') + '}'
+        print 'int n[%d] = %s;'%(len(input_list), input_list_str)
+        print 'vector<int> v(n, n+%d);' %(len(input_list))
+        
+        
+if __name__ == "__main__":
+    input_list = [27,2,-32,38]
+    convetr = list2vector()
+    convetr.int2int(input_list)
