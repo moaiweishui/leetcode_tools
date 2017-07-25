2e776cd0ce81daa011188ff1af7083d3ab62307c
LiuSS <liuyuanxiu1218@gmail.com>
2017-07-25 22:59:49 +0800

    Update list2vector.py

--- list2vector.py
+++ list2vector.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 
-
+# Convert list which is represented in text form to vector(initial by C++ code)
 class list2vector():
     def __init__(self):
         pass
-        
+    # Int list to int vector    
     def int2int(self, input_list):
         input_list_str = '{' + str(input_list).strip('[').strip(']') + '}'
         print 'int n[%d] = %s;'%(len(input_list), input_list_str)
